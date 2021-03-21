1ba25e96c286a9bbd2a800822121e44f0938a9ad
Igor Turchin <brainsplit.g@gmail.com>
2021-03-21 20:16:30 +0300

    Пятнашки

---
+++ Barley-Break/Cube.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Barley_Break
+{
+    class Cube
+    {
+        #region Const
+
+        /// <summary>
+        /// Толщина сетки
+        /// </summary>
+        public static readonly int MeshThickness = 2;
+
+        /// <summary>
+        /// Кисть кубика
+        /// </summary>
+        private static readonly Brush BrushCube = Brushes.DarkBlue;
+
+        /// <summary>
+        /// Кисть для рамки кубика с активным фокусом
+        /// </summary>
+        private static readonly Brush BorderIsFocusColor = Brushes.LightGreen;
+
+        /// <summary>
+        /// Кисть для рамки кубика с не атикным фокусом
+        /// </summary>
+        private static readonly Brush BorderNoFocusColor = Brushes.Transparent;
+
+        /// <summary>
+        /// Кисть для текста кубика не на своей позиции
+        /// </summary>
+        private static readonly Brush ForegroundTextNoPos = Brushes.White;
+
+        /// <summary>
+        /// Кисть для текста кубика на своей позиции
+        /// </summary>
+        private static readonly Brush ForegroundTextIsPos = Brushes.Orange;
+
+        #endregion
+
+        #region Property
+
+        /// <summary>
+        /// Ширина контейнера кубика
+        /// </summary>
+        public static double Width = 80;
+
+        /// <summary>
+        /// Высота контейнера кубика
+        /// </summary>
+        public static double Height = 70;
+
+        /// <summary>
+        /// Максимальное количество элементов в строке
+        /// </summary>
+        public static int MaxCountLine { get; set; }
+
+        /// <summary>
+        /// Номер кубика
+        /// </summary>
+        public int Num { get; set; }
+
+        /// <summary>
+        /// Визуальный нтерфейс
+        /// </summary>
+        public Grid Content { get; }
+
+        /// <summary>
+        /// Свойство задающее состояние фокуса кубика
+        /// </summary>
+        public bool Focused
+        {
+            set
+            {
+                Content.Background =
+                    value ?
+                    BorderIsFocusColor :
+                    BorderNoFocusColor;
+            }
+        }
+
+        /// <summary>
+        /// Позиция по "X"
+        /// </summary>
+        public int X
+        {
+            get => x;
+            set
+            {
+                x = value;
+                var pos_x = x * Width + x * MeshThickness;
+                Canvas.SetLeft(Content, pos_x);
+                UpDatePos();
+            }
+        }
+        private int x;
+
+        /// <summary>
+        /// Позиция по "Y"
+        /// </summary>
+        public int Y
+        {
+            get => y;
+            set
+            {
+                y = value;
+                var pos_y = y * Height + y * MeshThickness;
+                Canvas.SetTop(Content, pos_y);
+                UpDatePos();
+            }
+        }
+        private int y;
+
+        #endregion
+
+        public Cube(int num, int x, int y)
+        {
+            Num = num;
+            cube = CreateCube();
+            text = CreateText(num);
+            Content = CreateContent(cube, text);
+            X = x;
+            Y = y;
+        }
+
+        public event EventHandler GotFocus;
+        public event EventHandler LostFocus;
+        public event EventHandler Click;
+
+        public delegate void EventHandler(object sender, EventArgs e);
+
+        #region Private
+
+        private Rectangle cube { get; }
+        private TextBlock text { get; }
+
+        /// <summary>
+        /// Обновление текста при смене позиции
+        /// </summary>
+        private void UpDatePos()
+        {
+            if (Num == ((y) * MaxCountLine) + (x + 1))
+            {
+                text.Foreground = ForegroundTextIsPos;
+            }
+            else
+            {
+                text.Foreground = ForegroundTextNoPos;
+            }
+        }
+
+        /// <summary>
+        /// Создание кубика
+        /// </summary>
+        /// <returns></returns>
+        private Rectangle CreateCube()
+        {
+            return new Rectangle
+            {
+                Fill = BrushCube,
+                Margin = new Thickness(1),
+                RadiusX = 15,
+                RadiusY = 15,
+
+            };
+        }
+
+        /// <summary>
+        /// Создание текта кубика
+        /// </summary>
+        /// <param name="content">Отображаемый номер</param>
+        /// <returns></returns>
+        private TextBlock CreateText(int content)
+        {
+            var text = new TextBlock
+            {
+                Text = content.ToString(),
+                FontSize = 30,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+
+            return text;
+        }
+
+        /// <summary>
+        /// Создание визуального контента
+        /// </summary>
+        /// <typeparam name="T">Визуальный элемент/typeparam>
+        /// <typeparam name="V">визуальный элемент</typeparam>
+        /// <param name="cube">Кубик</param>
+        /// <param name="text">Текст кубика</param>
+        /// <returns></returns>
+        private Grid CreateContent<T, V>(T cube, V text)
+            where T : UIElement
+            where V : UIElement
+        {
+            var content = new Grid
+            {
+                Height = Height,
+                Width = Width
+            };
+
+            content.Children.Add(cube);
+            content.Children.Add(text);
+
+            content.MouseEnter += (s, e) =>
+            {
+                if (s is Grid rec)
+                {
+                    var handler = GotFocus;
+                    handler?.Invoke(this, new EventArgs());
+                }
+            };
+
+            content.MouseLeave += (s, e) =>
+            {
+                if (s is Grid rec)
+                {
+                    var handler = LostFocus;
+                    handler?.Invoke(this, new EventArgs());
+                }
+            };
+
+            content.MouseLeftButtonUp += (s, e) =>
+            {
+                if (s is Grid rec)
+                {
+                    var handler = Click;
+                    handler?.Invoke(this, new EventArgs());
+                }
+            };
+
+            return content;
+        }
+
+        #endregion
+    }
+}
---
+++ Barley-Break/Game.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Barley_Break
+{
+    static class Game
+    {
+        static bool[,] cubes;
+
+        /// <summary>
+        /// Запуск игры
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="container"></param>
+        /// <param name="CountX"></param>
+        /// <param name="CountY"></param>
+        static public void Start<T>(T container, int CountX, int CountY)
+            where T : Panel
+        {
+            container.Children.Clear();
+
+            cubes = new bool[CountX, CountY];
+            Cube.MaxCountLine = CountX;
+
+            Cube.Width = 500 / CountX - Cube.MeshThickness;
+            Cube.Height = 500 / CountY - Cube.MeshThickness;
+
+            var x = 0;
+            var y = 0;
+
+            foreach (var num in GetRandomNumber((CountX * CountY) - 1))
+            {
+                cubes[x, y] = true;
+                var cube = new Cube(num, x, y);
+                cube.GotFocus += EventGotFocus;
+                cube.LostFocus += EventLostFocus;
+                cube.Click += EventClick;
+                container.Children.Add(cube.Content);
+
+                if (++x == cubes.GetLength(0))
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+        }
+
+        public static event Action Step;
+
+        /// <summary>
+        /// Обработчик потери фокуса кубиком
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void EventLostFocus(object sender, EventArgs e)
+        {
+            if (sender is Cube cube)
+            {
+                cube.Focused = false;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик получения фокуса кубиком
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void EventGotFocus(object sender, EventArgs e)
+        {
+            if (sender is Cube cube)
+            {
+                cube.Focused = true;
+            }
+        }
+
+        /// <summary>
+        /// Проверка свободного места для возможности перемещения в заданную ячейку
+        /// </summary>
+        /// <param name="x">Позиция по "X"</param>
+        /// <param name="y">Позиция по "Y"</param>
+        /// <returns></returns>
+        private static bool IsFree(int x, int y)
+        {
+            if ((x >= 0) &&
+                (y >= 0) &&
+                (x < cubes.GetLength(0)) &&
+                (y < cubes.GetLength(1)))
+            {
+                return cubes[x, y] == false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Обработчик клика на кубик, перемещение кубика
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void EventClick(object sender, EventArgs e)
+        {
+            if (sender is Cube cube)
+            {
+                var x = cube.X;
+                var y = cube.Y;
+                var step = false;
+
+                if (IsFree(cube.X + 1, cube.Y))
+                {
+                    x++;
+                    step = true;
+                }
+                else if (IsFree(cube.X - 1, cube.Y))
+                {
+                    x--;
+                    step = true;
+                }
+                else if (IsFree(cube.X, cube.Y + 1))
+                {
+                    y++;
+                    step = true;
+                }
+                else if (IsFree(cube.X, cube.Y - 1))
+                {
+                    y--;
+                    step = true;
+                }
+
+                cubes[cube.X, cube.Y] = false;
+                cubes[x, y] = true;
+                cube.X = x;
+                cube.Y = y;
+
+                if (step)
+                    Step.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Получение случайных номеров для кубиков
+        /// </summary>
+        /// <param name="max">Количество кубиков</param>
+        /// <returns></returns>
+        private static IEnumerable<int> GetRandomNumber(int max)
+        {
+            var random = new Random();
+            var nums = new List<int>();
+
+            for (int i = 1; i < max + 1; i++)
+            {
+                nums.Add(i);
+            }
+
+            for (int i = 0; i < max; i++)
+            {
+                var index = random.Next(0, nums.Count - 1);
+                var num = nums[index];
+                nums.RemoveAt(index);
+
+                yield return num;
+            }
+        }
+    }
+}
